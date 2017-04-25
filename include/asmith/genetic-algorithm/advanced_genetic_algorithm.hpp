0c303874375a0042a147a8d620163b3583dbb678
SolaireLibrary <solairelibrary@mail.com>
2017-04-25 16:26:06 +0100

    Added uniform crossover

--- include/asmith/genetic-algorithm/advanced_genetic_algorithm.hpp
+++ include/asmith/genetic-algorithm/advanced_genetic_algorithm.hpp
@@ -26,11 +26,15 @@ protected:
 		return tmp;
 	}
 
+	// Selection
+	
 	inline const genome_t& select_random(const genome_t* const aGenomes, const size_t aSize) const throw() {
 		return aGenomes[generate_random() % aSize];
 	}
 
-	inline void mutation_replacement(genome_t& aGenome, const uint8_t aChance, typename genome_t::gene_t aValue) const throw() {
+	// Mutation
+	
+	void mutation_replacement(genome_t& aGenome, const uint8_t aChance, typename genome_t::gene_t aValue) const throw() {
 		const size_t s = aGenome.get_gene_count();
 		typename::genome_t::gene_t g* const = aGenome.get_genes();
 		for(size_t i = 0; i < s; ++i) {
@@ -38,13 +42,35 @@ protected:
 		}
 	}
 
-	inline void mutation_perturbation(genome_t& aGenome, const uint8_t aChance, typename genome_t::gene_t aStep) const throw() {
+	void mutation_perturbation(genome_t& aGenome, const uint8_t aChance, typename genome_t::gene_t aStep) const throw() {
 		const size_t s = aGenome.get_gene_count();
 		typename::genome_t::gene_t g* const = aGenome.get_genes();
 		for(size_t i = 0; i < s; ++i) {
 			if(generate_random() % 100 < aChance) g[i] += generate_random() % 100 < 50 ? -aStep : aStep;
 		}
 	}
+	
+	// Crossover
+	
+	void crossover_uniform(genome_t& aGenome, typename genome_t::gene_t aValue) const throw() {
+		const size_t p = get_parent_count();
+		const size_t s = aGenome.get_gene_count();
+		typename::genome_t::gene_t g* const = aGenome.get_genes();
+		const uint8_t increment = 100 / p;
+		
+		for(size_t i = 0; i < s; ++i) {
+			uint8_t chance = generate_random() % 100;
+			for(size_t j = 0; j < p; ++j) {
+				if(chance <= increment) {
+					g[i] = mParents[j].get_genes()[i];
+					break;
+				}else {
+					chance -= increment;
+				}
+			}
+		}
+	}
+	
 public:
 	advanced_genetic_algorithm() {
 	
